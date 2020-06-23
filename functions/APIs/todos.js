4bc5f991fb7765f1a825306964d57d9f97ce2ce6
yukta-patel <yuktapatel.infynno@gmail.com>
2020-06-23 15:52:21 +0530

    commited changes

---
+++ functions/APIs/todos.js
@@ -0,0 +1,27 @@
+const { db } = require('../util/admin');
+
+exports.getAllTodos = (request, response) => {
+	db
+		.collection('todos')
+		.orderBy('createdAt', 'desc')
+		.get()
+		.then((data) => {
+			
+			let todos = [];
+			data.forEach((doc) => {
+				console.log(doc);
+				todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+					body: doc.data().body,
+					createdAt: doc.data().createdAt,
+
+				});
+			});
+			return response.json(todos);
+		})
+		.catch((err) => {
+			console.error(err);
+			return response.status(500).json({ error: err.code});
+		});
+};
