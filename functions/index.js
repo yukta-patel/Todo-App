03eecde31825850ef238f89deac475c810624c29
yukta-patel <yuktapatel.infynno@gmail.com>
2020-06-26 11:35:06 +0530

    commited changes

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,35 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
+const auth = require('./util/auth');
+const { 
+    loginUser,
+    signUpUser,
+    uploadProfilePhoto,
+    getUserDetail,
+    updateUserDetails
+} = require('./APIs/users')
+
+
+
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+
+
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+app.post('/user/image', auth, uploadProfilePhoto);
+app.get('/user', auth, getUserDetail);
+app.post('/user', auth, updateUserDetails);
+
 exports.api = functions.https.onRequest(app);
+
+
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCCwD0PS_hfg8W6TsTgUDqpFChOhYKIYkg",
+  authDomain: "todo-infynno.firebaseapp.com",
+  databaseURL: "https://todo-infynno.firebaseio.com",
+  projectId: "todo-infynno",
+  storageBucket: "todo-infynno.appspot.com",
+  messagingSenderId: "545599591671",
+  appId: "1:545599591671:web:020547088cc345fd5daff2",
+  measurementId: "G-R060DDCEZW"
+};
